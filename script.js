1139f811660d08ace99b05d0001ad5c7710f24d8
AdRod14 <adan.rodarte@uabc.edu.mx>
2026-06-09 13:35:55 -0700

    added slider functionality

--- script.js
+++ script.js
@@ -252,6 +252,95 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTargets.forEach(img => imgObserver.observe(img));
 
 
+
+////////////////////////////////////////////////////////////
+// Slider component
+
+const slider = function() {
+
+  const slides = document.querySelectorAll('.slide');
+  const slider = document.querySelector('.slider');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+
+  let currSlide = 0;
+  const maxSlide = slides.length;
+
+
+  const createDots = function() {
+    slides.forEach(function(_,i){
+      dotContainer.insertAdjacentHTML('beforeend',`<button class="dots__dot" data-slide="${i}"></button>`);
+    });
+  };
+
+
+  const activateDot = function(slide) {
+    document.querySelectorAll('.dots__dot').forEach( dot => dot.classList.remove('dots__dot--active'));
+    document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+  }
+
+
+  const goToSlide = function(slide) {
+    slides.forEach((s, i) => s.style.transform = `translateX(${(i - slide) * 100}%)`);
+  }
+
+
+  const nextSlide = function() {
+    if(currSlide === (maxSlide - 1)){
+      currSlide = 0;
+    } else {
+      currSlide++;
+    }
+    goToSlide(currSlide);
+    activateDot(currSlide);
+  }
+
+
+  const prevSlide = function() {
+    if (currSlide === 0){
+      currSlide = maxSlide - 1;
+    }else {
+      currSlide--;
+    }
+    goToSlide(currSlide)
+    activateDot(currSlide);
+  }
+
+  const init = function () {
+    createDots();
+    goToSlide(0);
+    activateDot(0);
+  }
+
+  init()
+
+  // Event handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+  document.addEventListener('keydown', function(e) {
+    if(e.key === 'ArrowLeft') {
+      prevSlide();
+    }
+    if (e.key === 'ArrowRight') {
+      nextSlide();
+    }
+  })
+
+  dotContainer.addEventListener('click', function(e) {
+    if (e.target.classList.contains('dots__dot')){
+      currSlide = Number(e.target.dataset.slide);
+      goToSlide(currSlide);
+      activateDot(currSlide);
+    }
+  })
+}
+
+slider();
+
+
+
+
 /*
 //////////////////// SELECTING, CREATING AND DELETING ELEMENTS ////////////////////
 
